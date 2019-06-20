9ba33c6ff14ccbac9afad66390bd2274f94e52fd
tengfeilong <863080579@qq.com>
2019-06-20 17:45:24 +0800

    Signed-off-by: tengfeilong <863080579@qq.com>

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
 
-
+        Mews\Captcha\CaptchaServiceProvider::class,
 
     ],
 
@@ -231,7 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
     ],
 
 ];
