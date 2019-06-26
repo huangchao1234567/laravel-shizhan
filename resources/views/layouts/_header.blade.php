091c674782560e14e73693ecf474eb2988587ac7
tengfeilong <863080579@qq.com>
2019-06-26 14:53:20 +0800

    编辑 个人中心

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -52,9 +52,9 @@
                         </ul>
 
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="">个人中心</a>
+                            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="">编辑资料</a>
+                            <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" id="logout" href="#">
                                 <form action="{{ route('logout') }}" method="POST">
--- resources/views/shared/_error.blade.php
+++ resources/views/shared/_error.blade.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: john
- * Date: 2019/6/26
- * Time: 10:46
- */
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <div class="mt-2"><b>有错误发生：</b></div>
+        <ul class="mt-2 mb-2">
+            @foreach ($errors->all() as $error)
+                <li><i class="glyphicon glyphicon-remove"></i> {{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
