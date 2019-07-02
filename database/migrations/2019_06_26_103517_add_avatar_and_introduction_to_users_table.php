9f944fff4f91729498bd18c199caa69b4d1ca62b
tengfeilong <863080579@qq.com>
2019-07-02 11:09:21 +0800

    填充用户和话题数据

--- database/migrations/2019_06_26_103517_add_avatar_and_introduction_to_users_table.php
+++ database/migrations/2019_06_26_103517_add_avatar_and_introduction_to_users_table.php
@@ -14,8 +14,8 @@ class AddAvatarAndIntroductionToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //$table->string('avatar')->nullable();
-          //  $table->string('introduction')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
         });
     }
 
